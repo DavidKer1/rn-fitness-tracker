1dcd82c6e27a09c5d74f79a91f8c8a639dc45e70
Victor David Sanchez Garcia <macbook@Victors-MacBook-Air.local>
2022-05-24 22:00:28 -0500

    feat: Clean Architecture folders structure

---
+++ screens/WelcomeScreen.tsx
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+type Props = {}
+
+const WelcomeScreen = (props: Props) => {
+  return (
+    <View>
+      <Text>WelcomeScreen</Text>
+    </View>
+  )
+}
+
+export default WelcomeScreen
